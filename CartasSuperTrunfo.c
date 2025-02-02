1d74a5114534d7d79c961bc1737e9864a34ddf76
Juliano GS <gsilva.juliano@outlook.com>
2025-02-02 19:24:36 +0000

    Apresentação dos dados coletados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,7 +21,7 @@ int main() {
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
 
-    printf("---- Cadastro de Cartas Super Trunfo Cidades ---- \n\n");
+    printf("---- Super Trunfo Cidades | Cadastro de Cartas ---- \n\n");
     printf("Escolha uma letra entre A ate H para iniciar: \n");
     scanf("%c",&estado);
     printf("Escolha um número entre 1 a 4: \n");
@@ -36,12 +36,23 @@ int main() {
     scanf("%f",&pib);
     printf("Qual é a quantidade de pontos turisticas desta cidade? \n");
     scanf("%d",&pontosturisticos);
+    printf("--- Cadastrando ---\n");
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("****\n");
+    printf("*****\n");
 
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.*/
-
-    //* printf("Carta: %c%s",estado,codigodacarta);
-
+    printf("---- Super Trunfo Cidades | Carta cadastrada com sucesso ---- \n\n");
+    printf("Carta: %c%s \n",estado,codigodacarta);
+    printf("Cidade: %s \n",nomedacidade);
+    printf("População: %d \n",populacao);
+    printf("Área em Km²: %f \n",area);
+    printf("PIB: %f Reais. \n",pib);
+    printf("Quantidade de pontos turisticos: %d \n\n",pontosturisticos);
+    printf("---- Super Trunfo Cidades ---- \n\n");
 return 0;
 }
